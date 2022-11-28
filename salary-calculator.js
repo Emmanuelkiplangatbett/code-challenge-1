8d80733cdbe44440065ea5585d7f9ab3382b351b
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-11-28 19:09:23 +0300

    added salary-calculat0r

--- salary-calculator.js
+++ salary-calculator.js
@@ -1,5 +1,44 @@
-// user to input the basic salary and total benefits
-const net =require(prompt-sync)
+const net = require("prompt-sync");
+const prompt = net();
+let salary = prompt("write your income");
+function taxRate(){
+    if(salary <= 24000){
+        return(salary/100)*10;
+    }
+    else if(salary > 24000 && salary <= 32333){
+        return(salary/100)*25;
+    }
+    else{
+        return(salary/100)*30;
+    }
+}
+let tax = taxRate();
+function medicalCover(){
+    if (salary < 100000){
+        return salary*(1.6/100);
+    }
+    else{
+        return 1700
+    }
+}
+let NHIF = medicalCover();
+function nationalSecurity(){
+    if(salary <= 100000){
+        return salary*(6/100);
+    }
+    else{
+        return 6000;
+    }
+}
+let NSSF = nationalSecurity();
+console.log(`Gross Salary: Ksh.${salary}`);
+console.log(`Tax:Ksh.${tax}`);
+console.log(`NHIF Deductions: Ksh.${NHIF}`);
+console.log(`NSSF Deductions: Ksh.${NSSF}`);
+let netSalary = salary-(tax + NSSF + NHIF);
+console.log(`Net Salary: Ksh.${netSalary}`);
+
+
 
 
 
